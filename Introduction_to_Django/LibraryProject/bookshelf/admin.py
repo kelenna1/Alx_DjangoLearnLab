ab6985a397a07160c49fd92e3e1c50fc960402ab
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-11-03 22:43:51 +0100

    admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Fields to display in list view
+    list_filter = ('author', 'publication_year')  # Filters for quick navigation
+    search_fields = ('title', 'author')  # Searchable fields for fast lookup
+
+admin.site.register(Book, BookAdmin)
+
