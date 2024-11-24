22e272de86ceade0db54753527ec19807cbedb31
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-11-24 20:09:18 +0100

    why abeg

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,6 @@ from .serializers import BookSerializer
 
 # Create your views here.
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
