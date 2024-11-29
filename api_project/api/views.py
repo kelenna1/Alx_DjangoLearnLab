925bff8594524ab9ef0044105205876afa0ed070
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-11-29 17:25:32 +0100

    generics import

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import generics, viewsets
 # Create your views here.
 
 class BookList(generics.ListAPIView):
