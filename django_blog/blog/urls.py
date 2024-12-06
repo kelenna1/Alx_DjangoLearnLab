0c719e3e03de7cecce8eda8749d766aa1c931ada
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-12-06 12:54:25 +0100

    task 2 prime

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('home', views.home, name="home" ),
-    path("login", LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("posts", views.posts, name= "posts"),
-    path("register", views.register, name="register"),
+    path("register/", views.register, name="register"),
     path('logout', LogoutView.as_view(next_page = "/"), name="logout"),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
