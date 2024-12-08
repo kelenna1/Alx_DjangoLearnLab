c20d3680375a286a1b8b603af99d8dde3fa1bff8
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-12-08 20:46:15 +0100

    abegggk

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -20,7 +20,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control"}),
             "content": forms.Textarea(attrs={"class": "form-control"}),
-             "tags": TagWidget, 
+             "tags": TagWidget(), 
         }
 
 class CommentForm(forms.ModelForm):
