860d4505cefa4d77e7bfd5a0d5863a86d9983ac6
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-12-12 15:39:03 +0100

    task 1

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,29 +5,33 @@ from django.contrib.auth import authenticate
 
 User = get_user_model()
 
-class UserRegistrationSerializer(serializers.Serializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):  # Inherit from ModelSerializer
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'password2e', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only':True}}
+        fields = ['id', 'email', 'username', 'password', 'bio', 'profile_picture']  # Fixed "password"
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        # Use create_user to create a new user
         user = User.objects.create_user(
-            username= validated_data['username'],
-            email = validated_data.get['email', ''],
-            password = validated_data['password'],
-            bio = validated_data.get['bio', ''],
-            profile_picture = validated_data.get['profile_picture', None]
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],  # Fixed parenthesis for get
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
+        # Create a token for the user
         Token.objects.create(user=user)
         return user
-    
-class USerLoginSerializer(serializers.Serializer):
+
+
+class UserLoginSerializer(serializers.Serializer):  # Fixed typo in class name
     username = serializers.CharField()
-    password = serializers.CharField(write_only = True)
+    password = serializers.CharField(write_only=True)
 
     def validate(self, data):
+        # Authenticate the user
         user = authenticate(username=data['username'], password=data['password'])
         if user and user.is_active:
             return user
-        raise serializers.ValidationError("invalid credentials")
+        raise serializers.ValidationError("Invalid credentials")
