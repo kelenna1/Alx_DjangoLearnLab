2da967032f3540c78ac02e8b22334d6715f83ad7
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-12-13 09:51:04 +0100

    task 22

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    path('api/', include('posts.urls'))
+
 ]
